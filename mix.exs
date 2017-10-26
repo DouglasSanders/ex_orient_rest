db65936833e26128a5a8a1a1bf00587a980f0e84
DouglasSanders <douglas.johman.sanders@gmail.com>
2017-10-26 10:29:41 +0800

    Mixfile

--- mix.exs
+++ mix.exs
@@ -7,22 +7,38 @@ defmodule ExOrientRest.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:httpoison, "~> 0.8 or ~> 0.9 or ~> 0.10"},
+      {:poison, "~> 1.5 or ~> 2.0 or ~> 3.0"}
+    ]
+  end
+
+  defp description do
+    """
+    REST interface to OrientDB.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Douglas Sanders"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/DouglasSanders/ex_orient_rest"}
     ]
   end
 end
