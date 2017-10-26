6a8e439c1c88f33d0304440fe4caf2d65ff985b5
DouglasSanders <douglas.johman.sanders@gmail.com>
2017-10-26 10:23:26 +0800

    Mix New

---
+++ lib/ex_orient_rest.ex
@@ -0,0 +1,18 @@
+defmodule ExOrientRest do
+  @moduledoc """
+  Documentation for ExOrientRest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExOrientRest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
